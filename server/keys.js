29a6474dadac726df39e23a2e1c8e984aaca1acd
vrvwebinterns <vrvwebinterns@gmail.com>
2019-03-20 18:33:12 +0530

    added

--- server/keys.js
+++ server/keys.js
@@ -1,19 +1,19 @@
-module.exports={
-    redisHost: 'localhost',
-    redisPort: '6379',
-    pgUser: 'postgres',
-    pgHost: 'localhost',
-    pgDatabase: 'demo',
-    pgPassword: 'root',
-    pgPort: '5432'
-};
-
 // module.exports={
-//     redisHost: process.env.REDIS_HOST,
-//     redisPort: process.env.REDIS_PORT,
-//     pgUser: process.env.PGUSER,
-//     pgHost: process.env.PGHOST,
-//     pgDatabase: process.env.PGDATABASE,
-//     pgPassword: process.env.PGPASSWORD,
-//     pgPort: process.env.PGPORT
+//     redisHost: 'localhost',
+//     redisPort: '6379',
+//     pgUser: 'postgres',
+//     pgHost: 'localhost',
+//     pgDatabase: 'demo',
+//     pgPassword: 'root',
+//     pgPort: '5432'
 // };
+
+module.exports={
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PGUSER,
+    pgHost: process.env.PGHOST,
+    pgDatabase: process.env.PGDATABASE,
+    pgPassword: process.env.PGPASSWORD,
+    pgPort: process.env.PGPORT
+};
